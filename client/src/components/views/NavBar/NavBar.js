4b7f62b5203898a908833843cce6684ffeab04f4
root <dongha0940@naver.com>
2022-03-06 16:18:34 +0900

    boiler-plate 파일 구조 정리

---
+++ client/src/components/views/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar(){
+    return (
+        <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+    )
+}
+export default NavBar
